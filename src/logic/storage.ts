7c41475c774fb5a4a565748ae5988ce02f28459c
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2022-02-13 22:10:54 +0530

    ft: popup structure

--- src/logic/storage.ts
+++ src/logic/storage.ts
@@ -1,3 +1,12 @@
-import { useStorageLocal } from '~/composables/useStorageLocal'
+import { useStorageLocal } from "~/composables/useStorageLocal";
 
-export const storageDemo = useStorageLocal('webext-demo', 'Storage Demo', { listenToStorageChanges: true })
+export const storageDemo = useStorageLocal("webext-demo", "Storage Demo", {
+  listenToStorageChanges: true,
+});
+
+export const username = useStorageLocal("username", null); // testing
+
+export const userDoc = useStorageLocal("userDoc", null, {
+  listenToStorageChanges: true,
+  deep: true,
+});
--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,23 +1,39 @@
 <template>
   <main class="w-[300px] px-4 py-5 text-center text-gray-700">
     <Logo />
-    <div>Popup</div>
-    <p class="mt-2 opacity-50">
-      This is the popup page
-    </p>
-    <button class="btn mt-2" @click="openOptionsPage">
-      Open Options
-    </button>
-    <div class="mt-2">
-      <span class="opacity-50">Storage:</span> {{ storageDemo }}
+    <div>MetaPass</div>
+    <div v-if="userDoc">
+      <button class="btn mt-2" @click="openOptionsPage">Launch Vault</button>
+      <div class="mt-2 text-teal-700">
+        <span class="hover:underline cursor-pointer">Switch account</span> |
+        <span class="hover:underline cursor-pointer" @click="logout"
+          >Logout</span
+        >
+      </div>
+    </div>
+    <div class="space-x-1" v-else>
+      <button class="btn mt-2" @click="loginOrRegister">Login</button>
+      <button class="btn-inverted mt-2" @click="loginOrRegister">Register</button>
     </div>
   </main>
 </template>
 
 <script setup lang="ts">
-import { storageDemo } from '~/logic/storage'
+import { userDoc } from "~/logic/storage";
 
 function openOptionsPage() {
-  browser.runtime.openOptionsPage()
+  browser.runtime.openOptionsPage();
+}
+
+function logout() {
+  userDoc.value = null;
+}
+
+function loginOrRegister() {
+  userDoc.value = {
+    username: "neelansh",
+    masterPassword: "ereeffe",
+  };
+  console.log("login or register");
 }
 </script>
