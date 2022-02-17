9be2788f30da3cac1f2901673ac41b21f58e927a
Vedant Mahadik <61352323+VedantMahadik@users.noreply.github.com>
2022-02-17 15:41:20 +0530

    ft: Add api endpoints for getPass, addPass and updatePass

--- src/composables/useAPI.ts
+++ src/composables/useAPI.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { API_URL } from "~/constants";
+import { userDoc } from "~/logic/storage";
 
 const getSites = async (id = "vedant32") => {
   const { data, status } = await axios.post(API_URL + "/getAllPass", {
@@ -36,5 +37,50 @@ const register = async (email: String, password: String) => {
     return [];
   }
 };
+const addPass = async (website: String, username: String, password: String) => {
+  const { data, status } = await axios.post(API_URL + "/addPass", {
+    id: userDoc.username,
+    website: website,
+    username: username,
+    password: password,
+    key: userDoc.masterPassword,
+  });
+  if (status === 200) return data;
+  else {
+    console.error("Error while fetching user's sites");
+    return [];
+  }
+};
+const updatePass = async (
+  website: String,
+  username: String,
+  password: String,
+) => {
+  const { data, status } = await axios.post(API_URL + "/updatePass", {
+    id: userDoc.username,
+    website: website,
+    username: username,
+    password: password,
+    key: userDoc.masterPassword,
+  });
+  if (status === 200) return data;
+  else {
+    console.error("Error while fetching user's sites");
+    return [];
+  }
+};
+const getPass = async (website: String, username: String) => {
+  const { data, status } = await axios.post(API_URL + "/getPass", {
+    id: userDoc.username,
+    website: website,
+    username: username,
+    key: userDoc.masterPassword,
+  });
+  if (status === 200) return data;
+  else {
+    console.error("Error while fetching user's sites");
+    return [];
+  }
+};
 
-export { getSites, login, register };
+export { getSites, login, register, addPass, updatePass, getPass };
